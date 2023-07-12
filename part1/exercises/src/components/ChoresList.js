d7bf91eda949e638222b782f5087b2fd788cb59f
SarahLakey <136642286+SarahLakey@users.noreply.github.com>
2023-07-12 12:37:46 -0500

    Part 1 of the exercises completed

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   // return <p>No content is here!</p>;
+   
+   return (
+      <div>
+         <h3 className = {classes.choresHeading}>Chores List That Never Ends</h3>
+         <ol>
+            <li className={classes.choresText}>Laundry</li>
+            <li className={classes.choresText}>Dishes</li>
+         </ol>
+      </div>
+      );
+   
 }
