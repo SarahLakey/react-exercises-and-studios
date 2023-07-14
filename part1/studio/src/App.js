530b6dc9e86fee0b17061cd751a0595501aa1c07
SarahLakey <136642286+SarahLakey@users.noreply.github.com>
2023-07-13 21:42:11 -0500

    finished receipe photo component

--- part1/studio/src/App.js
+++ part1/studio/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import RecipeDescription from './components/Description';
 import RecipeIngredients from './components/Ingredients';
+import RecipePhoto from './components/Photos';
 
 function App() {
   return (
     <div className="App">
-    <div>
-      <RecipeDescription />
-      <RecipeIngredients />
-   </div>
+      <RecipePhoto />
+      <div>
+        <RecipeDescription />
+        <RecipeIngredients />
+      </div>
     </div>
   );
 }
