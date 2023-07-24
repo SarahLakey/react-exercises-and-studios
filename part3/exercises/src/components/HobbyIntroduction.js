9cc4cb0b1428b5e0b984ecc048bf4b8f7b090e7f
SarahLakey <136642286+SarahLakey@users.noreply.github.com>
2023-07-24 17:50:45 -0500

    Finished exercises for part 3

---
+++ part3/exercises/src/components/HobbyIntroduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+    return(
+     <div>
+         <h2>3 Fun Facts About Dog Walking</h2>
+         <ol>
+             <li>It is a way to explore all the local parks.</li>
+             <li>It lowers your stress level.</li>
+             <li>You will meet fellow dog lovers.</li>
+         </ol>
+     </div>
+    );
+ }
